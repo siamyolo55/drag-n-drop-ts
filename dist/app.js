e9d6cbeec2ffbaa69c3f5ada4c4758ae78d09759
Abrar Siam <abrarsiam@gmail.com>
2022-09-25 11:50:58 +0600

    getting used to typescript

--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,21 @@
 "use strict";
+class ProjectInput {
+    constructor() {
+        this.templateElement = document.getElementById('project-input');
+        this.hostElement = document.getElementById('app');
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild;
+        this.element.id = 'user-input';
+        this.titleInputElement = this.element.querySelector('#title');
+        this.descriptionInputElement = this.element.querySelector('#description');
+        this.peopleInputElement = this.element.querySelector('#people');
+        this.attach();
+    }
+    configure() {
+    }
+    attach() {
+        this.hostElement.insertAdjacentElement('afterbegin', this.element);
+    }
+}
+;
+const printInput = new ProjectInput();
---
+++ src/app.ts
@@ -0,0 +1,35 @@
+class ProjectInput {
+
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+    element:HTMLFormElement;
+    titleInputElement: HTMLInputElement;
+    descriptionInputElement: HTMLInputElement;
+    peopleInputElement: HTMLInputElement;
+
+    constructor() {
+        this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
+        this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild! as HTMLFormElement;
+        this.element.id = 'user-input';
+
+        this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
+        this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
+        this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
+ 
+        this.attach();
+    }
+
+    private configure() {
+
+    }
+
+
+    private attach() {
+        this.hostElement.insertAdjacentElement('afterbegin', this.element);
+    }
+};
+
+const printInput = new ProjectInput();
